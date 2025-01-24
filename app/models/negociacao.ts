7f90fcd4eb3b719ee80383d9f40abbe290b2ae5c
WictorBertulino <wictorbertulino@gmail.com>
2025-01-24 09:08:11 -0400

    finalizando o curso com a modelagem de domínio e melhorias no código, simplificando e deixando menos verboso

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,20 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    constructor(
+        private  _data: Date,
+        public readonly quantidade: number, 
+        public readonly valor: number
+    ){}
 
-    get data(): Date {
-        return this._data
-    }
-
-    get quantidade(): number {
-        return this._quantidade
-    }
-
-    get valor(): number {
-        return this._valor;
-    }
 
     get volume(): number {
-        return this._valor * this._quantidade;
+        return this.valor * this.quantidade;
+    }
+    // programação defensiva - O conceito diz aonde não 
+    // devo autorizar meu atributo do construtor em ser alterado.
+    // dessa forma crio uma cópia do meu atributo da classe e busco o valor dele retornando a cópia.
+    get data() : Date {
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,19 +1,17 @@
 export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get volume() {
-        return this._valor * this._quantidade;
+        return this.valor * this.quantidade;
+    }
+    // programação defensiva - O conceito diz aonde não 
+    // devo autorizar meu atributo do construtor em ser alterado.
+    // dessa forma crio uma cópia do meu atributo da classe e busco o valor dele retornando a cópia.
+    get data() {
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
